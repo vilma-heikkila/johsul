d6aad1bdb2822d094d68027a919d8631e088fcbd
bitofhope <bitofhope@kapsi.fi>
2021-11-18 21:46:03 +0200

    Refactor and clarify code

--- Embedded_Exercise_normal/src/Pixel.c
+++ Embedded_Exercise_normal/src/Pixel.c
@@ -5,107 +5,92 @@
  *      Author: -----
  */
 
+typedef _Bool bool; // TODO: where's my <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+
 #include "Pixel.h"
 
-#define ctrl *(uint8_t*) 0x41220008
-#define channel *(uint8_t*) 0x41220000
+// Table for pixel dots
+//                    height  width  RGB
+volatile uint8_t dots[DOTS_X][DOTS_Y][3] = {0};
 
-
-//Table for pixel dots.
-//				 dots[X][Y][COLOR]
-volatile uint8_t dots[8][8][3]={0};
-
+// Helper function for bit manipulation
+inline void set_bit(uint8_t* addr, uint8_t bit, bool value) {
+	if (value) {
+		*addr |= bit;
+	} else {
+		*addr &= ~bit;
+	}
+}
 
 // Here the setup operations for the LED matrix will be performed
-void setup(){
-	ctrl = 0;
-	channel = 0;
+void setup() {
+	*CTRL = 0x00;
+	*CHANNEL = 0x00;
 
-	//reseting screen at start is a MUST to operation (Set RST-pin to 1).
-	ctrl |= 0x01;	// RST is bit0  -> 0b00001
+	// Reset the LED matric by turning RST bit on and off
+	set_bit(CTRL, CTRL_RST, true);
 	usleep(500);
-
-	ctrl &=~ 0x01;
+	set_bit(CTRL, CTRL_RST, false);
 	usleep(500);
+	set_bit(CTRL, CRTL_RST, true);
 
-	ctrl |= 0x01;
+	// SDA controls the value being sent via serial line.
+	set_bit(CTRL, CTRL_SDA, true);
 
-	ctrl |= 0x10;  // SDA is bit4 -> 0b10001
-
-	//Write code that sets 6-bit values in register of DM163 chip. Recommended that every bit in that register is set to 1. 6-bits and 24 "bytes", so some kind of loop structure could be nice.
-	//24*6 bits needs to be transmitted
-
-	for(uint8_t data_counter = 0; data_counter < 144; data_counter++) {
-
-		ctrl &=~ 0x08; // bit3 to 0
-		ctrl |= 0x08; // bit3 to 1
+	// For each 24-bit channel in DM163, set the 6-bit register to 0b11111
+	for (size_t i = 0; i < DM163_CHANNELS * DM163_BANK0_BITS; i++) {
+		// DM163 reads SDA to shift register on rising edge of CSK
+		set_bit(CTRL, CTRL_CSK, false);
+		set_bit(CTRL, CTRL_CSK, true); // read
 	}
 
-	//Final thing in this function is to set SB-bit to 1 to enable transmission to 8-bit register.
-	ctrl |= 0x04;	// SB is bit2 -> 0b10101
+	// Set SB to 1 to enable transmission to 8-bit register.
+	set_bit(CTRL, CTRL_SB, true);
 }
 
-//Change value of one pixel at led matrix. This function is only used for changing values of dots array
-void SetPixel(uint8_t x,uint8_t y, uint8_t r, uint8_t g, uint8_t b){
-
-	//Hint: you can invert Y-axis quite easily with 7-y
-	dots[x][y][0]=b;
-	//Write rest of two lines of code required to make this function work properly (green and red colors to array).
-	dots[x][y][1]=g;
-	dots[x][y][2]=r;
+// Change value of one pixel at led matrix.
+// This function is only used for changing values of dots array
+void SetPixel(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b) {
+	dots[x][y][B]=b;
+	dots[x][y][G]=g;
+	dots[x][y][R]=r;
 }
 
+// Latch signal
+// Both shift registers are latched on falling edge of LAT
+void latch() {
+	set_bit(CTRL, CTRL_LAT, true);
+	set_bit(CTRL, CTRL_LAT, false); // latch
+}
 
-//Put new data to led matrix. Hint: This function is supposed to send 24-bytes and parameter x is for channel x-coordinate.
-void run(uint8_t x){
-	ctrl &=~ 0x02; // bit1 to 0
+// Put new data to led matrix. Hint: This function is supposed to send 24-bytes
+// and parameter x is for CHANNEL x-coordinate.
+void run(uint8_t x) {
+	set_bit(CTRL, CTRL_LAT, false);
 
-	//Write code that writes data to led matrix driver (8-bit data). Use values from dots array
-	//Hint: use nested loops (loops inside loops)
-	//Hint2: loop iterations are 8,3,8 (pixels,color,8-bitdata)
-	for(uint8_t i = 0; i < 8; i++) {
-
-		for(uint8_t k = 0; k < 3; k++) {
-			uint8_t colour_data = dots[x][i][k];
-
-			for(uint8_t j; j < 8; j++) {
-
-				if (colour_data & 0x80) ctrl|=0x10; // if bit8 in dots is set to 1...
-				else ctrl&=~0x10;
-
-				ctrl&=~0x08; // CLK to 0
-				ctrl|=0x08;  // CLK to 1
-				colour_data<<=1;
+	// Write code that writes data to led matrix driver (8-bit data). Use values from dots array
+	// Write colours from dots arrau to DM163
+	for (size_t y = 0; y < DOTS_Y; y++) {
+		for (size_t rgb_channel = 0; rgb_channel < 3; rgb++) {
+			uint8_t colour_data = dots[x][y][rgb_channel];
+			// Read colour to shift register
+			for (uint8_t i; i < DM163_BANK_BITS; i++) {
+				set_bit(CTRL, CTRL_SDA, colour_data & bit(8) != 0);
+				// Write SDA to shift register on rising edge of CLK
+				set_bit(CTRL, CTRL_CLK, false);
+				set_bit(CTRL, CTRL_CLK, true);
+				colour_data <<= 1;
 			}
 		}
 	}
-
 	latch();
 }
 
-//Latch signal. See colorsshield.pdf or DM163.pdf in project folder on how latching works
-void latch(){
-	ctrl |= 0x02; 	// Lat bit is bit1 -> 0b00010
-	ctrl &=~ 0x02;
-
-}
-
-
-//Set one line (channel) as active, one at a time.
+// Set one line (CHANNEL) as active, one at a time.
 void open_line(uint8_t x){
-	switch(x) {
-		case 0: channel|=0x01; // 0b00000001
-		case 1: channel|=0x02; // 0b00000010
-		case 2: channel|=0x04; // 0b00000100
-		case 3: channel|=0x08; // 0b00001000
-		case 4: channel|=0x10; // 0b00010000
-		case 5: channel|=0x20; // 0b00100000
-		case 6: channel|=0x40; // 0b01000000
-		case 7: channel|=0x80; // 0b10000000
-		default: channel = 0;
-	}
-
+	// If x>7, bit(x) overflows and is set to 0
+	*CHANNEL = bit(x);
 }
-
-
 
--- Embedded_Exercise_normal/src/Pixel.h
+++ Embedded_Exercise_normal/src/Pixel.h
@@ -16,8 +16,61 @@
 #include "xscugic.h"
 #include "xparameters.h"
 
+/*
+ * Macro definitions
+ */
 
+// LED controller specs
+#define DM163_CHANNELS 24
+#define DM163_BANK0_BITS 6
+#define DM163_BANK1_BITS 8
 
+// LED matrix specs
+#define DOTS_X 8
+#define DOTS_Y 8
+// Color channel order
+#define B 0
+#define G 1
+#define R 2
+
+/* Important memory addresses and bits */
+#define bit(n) (0x01 << n)
+
+// LED matrix channels
+#define CHANNEL ((uint8_t*) 0x41220008)
+
+// Control signals
+#define CTRL ((uint8_t*) 0x41220008)
+#define CTRL_RST bit(0)
+#define CTRL_LAT bit(1)
+#define CTRL_SB  bit(2)
+#define CTRL_SCK bit(3)
+#define CTRL_SDA bit(4)
+
+// Onboard buttons and switches
+#define INPUT ((uint8_t*) 0xe000a068)
+#define INPUT_BTN0 bit(0)
+#define INPUT_BTN1 bit(1)
+#define INPUT_BTN2 bit(2)
+#define INPUT_BTN3 bit(3)
+#define INPUT_SW0  bit(4)
+#define INPUT_SW1  bit(5)
+
+// Onboard monochrome LEDs
+#define BOARD_LED ((uint8_t*) 0x41200000)
+#define BOARD_LED0 bit(0)
+#define BOARD_LED1 bit(1)
+#define BOARD_LED2 bit(2)
+#define BOARD_LED3 bit(3)
+
+// Onboard RGB LEDs
+#define BOARD_LED_RGB
+#define BOARD_LED4_B bit(0)
+#define BOARD_LED4_G bit(1)
+#define BOARD_LED4_R bit(2)
+#define BOARD_LED5_B bit(3)
+#define BOARD_LED5_G bit(4)
+#define BOARD_LED5_R bit(5)
 
 void setup();
 void SetPixel(uint8_t x,uint8_t y, uint8_t r, uint8_t g, uint8_t b);
